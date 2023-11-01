b90df0389c63af10690d2528c59f05700770ed91
mhmdhussein377 <mohammad.hussein377@gmail.com>
2023-11-01 16:00:42 +0200

    connected to db

---
+++ lib/mongoose.ts
@@ -0,0 +1,24 @@
+import mongoose  from "mongoose";
+
+let isConnected = false
+
+export const connectDB = async () => {
+
+    mongoose.set("strictQuery", true)
+
+    if(!process.env.DATABASE_URL) return console.log("Missing MongoDB URL")
+
+    if(!isConnected) {
+        try {
+            await mongoose.connect(process.env.DATABASE_URL)
+
+            isConnected = true
+            console.log("MongoDB Connected")
+        } catch (error) {
+            console.log(error)
+        }
+        return
+    }
+
+    console.log("MongoDB connected already established")
+}
