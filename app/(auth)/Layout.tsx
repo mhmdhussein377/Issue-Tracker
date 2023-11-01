6d25c3f7b7781a46063b0c4a4c2d873452fa329b
mhmdhussein377 <mohammad.hussein377@gmail.com>
2023-11-01 17:08:30 +0200

    styled the signup page

--- app/(auth)/Layout.tsx
+++ app/(auth)/Layout.tsx
@@ -1,4 +1,3 @@
-import './globals.css'
 import type {Metadata}
 from 'next'
 import {Inter} from 'next/font/google'
@@ -14,10 +13,6 @@ export default function RootLayout({children} : {
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
-            <body className={inter.className}>
-                {children}
-            </body>
-        </html>
+        {children}
     )
 }
--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -14,6 +14,7 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
+        minLength: 8,
         required: true,
     },
 })
